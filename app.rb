83df69e5daffa448ae568b7635ec866a03e6a4f7
rootinginway <romeopanasak@gmail.com>
2018-01-11 14:32:50 +0200

    Set results as hash property for db object to true

--- app.rb
+++ app.rb
@@ -5,7 +5,9 @@ require 'sinatra/reloader'
 require 'sqlite3'
 
 def get_db
-	return SQLite3::Database.new 'barbershop.db'
+	db = SQLite3::Database.new 'barbershop.db'
+	db.results_as_hash = true
+	return db
 end
 
 configure do
